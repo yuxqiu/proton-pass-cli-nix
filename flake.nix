65a62ca72c0488bf7666cf184071373f53e90f06
tomsch <tom@sch.at>
2025-12-03 20:48:39 +0100

    flake: Enable allowUnfree for CI builds
    Fix GitHub Actions workflow failing with unfree license error.

--- flake.nix
+++ flake.nix
@@ -6,7 +6,10 @@
   outputs = { self, nixpkgs }: 
     let
       system = "x86_64-linux";
-      pkgs = nixpkgs.legacyPackages.${system};
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;
+      };
     in {
       packages.${system} = {
         default = pkgs.callPackage ./package.nix {};
